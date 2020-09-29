a2b65c1ae7a5e13dc8470a9e12a08789346039ff
Jon Robson <jonandrewrobson@gmail.com>
2020-09-29 05:55:53 -0500

    repo clean up

---
+++ c01/js/add-content.js
@@ -0,0 +1,16 @@
+var today = newDate();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+        greeting = "Good evening!";
+}   else if (hourNow < 18) {
+        greeting = "Good afternoon!";
+}   else if (hourNow > 0) {
+        greeting = "Good morning";
+}
+    else {
+        greeting = "Welcome!";
+}
+
+document.write('<h3>' + greeting + '</h3');
